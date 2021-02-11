294e36660c5f3a69b49c81986d18d7f7d1462b6f
Annie Huang <annie.huang@auspost.com.au>
2021-02-11 14:59:42 +1100

    Test <TextField> and <VNavigation>. Success.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import './App.css';
+import { LinkStatus, TextField, VNavigation } from 'otc-ui-component-library';
 
 function App() {
   return (
     <div>
-      Hello world
+      TextField
+      <TextField />
+      <hr />
+      <hr />
+      VNavigation:
+      <VNavigation
+        links={[
+          {
+            label: 'Getting started',
+            url: 'https://auspost.com.au',
+          },
+          {
+            label: 'Transfer details',
+            url: 'https://auspost.com.au',
+          },
+          {
+            label: 'Sender details',
+            url: 'https://auspost.com.au',
+          },
+          {
+            label: 'Receiver details',
+            status: LinkStatus.Active,
+            url: 'https://auspost.com.au',
+          },
+          {
+            label: 'Self-declared questions',
+            url: 'https://auspost.com.au',
+          },
+          {
+            label: 'Review & confirm',
+            url: 'https://auspost.com.au',
+          },
+        ]}
+      />
     </div>
   );
 }
