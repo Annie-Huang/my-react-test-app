9019b9407f6f80b1fae3c42e51d77f514581202a
Annie Huang <annie.huang@auspost.com.au>
2021-02-24 10:49:10 +1100

    Test Stepper - Success

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { LinkStatus, TextField, VNavigation } from 'otc-ui-component-library';
+import {
+  LinkStatus,
+  TextField,
+  VNavigation,
+  Stepper,
+  StepStatus,
+  Button,
+} from 'otc-ui-component-library';
+
+const longStepList = [
+  {
+    label: 'Step0',
+    status: StepStatus.InProgress,
+  },
+  {
+    label: 'Step1',
+    status: StepStatus.NotStarted,
+  },
+  {
+    label: 'Step2',
+    status: StepStatus.NotStarted,
+  },
+  {
+    label: 'Step3',
+    status: StepStatus.Disabled,
+  },
+  {
+    label: 'Step4',
+    status: StepStatus.NotStarted,
+  },
+  {
+    label: 'Step5',
+    status: StepStatus.Disabled,
+  },
+  {
+    label: 'Step6',
+    status: StepStatus.NotStarted,
+  },
+  {
+    label: 'Step7',
+    status: StepStatus.NotStarted,
+  },
+  {
+    label: 'Step8',
+    status: StepStatus.NotStarted,
+  },
+  {
+    label: 'Step9',
+    status: StepStatus.NotStarted,
+  },
+  {
+    label: 'Step10',
+    status: StepStatus.NotStarted,
+  },
+  {
+    label: 'Step11',
+    status: StepStatus.NotStarted,
+  },
+  {
+    label: 'Step12',
+    status: StepStatus.NotStarted,
+  },
+];
 
 function App() {
-  return (
+  /*  return (
     <div>
       TextField
       <TextField />
@@ -40,6 +102,39 @@ function App() {
         ]}
       />
     </div>
+  );*/
+
+  let initialSteps = [...longStepList];
+  const [steps, setSteps] = useState(initialSteps);
+
+  const setCurrentAndSubsequentSteps = (currentStepIndex: number) => {
+    const updatedSteps = steps.map((step, index) => {
+      return index === currentStepIndex
+        ? { ...step, status: StepStatus.InProgress }
+        : index > currentStepIndex && step.status !== StepStatus.Disabled
+        ? { ...step, status: StepStatus.NotStarted }
+        : { ...step, status: StepStatus.Completed };
+    });
+
+    setSteps(updatedSteps);
+  };
+
+  return (
+    <>
+      <div className='container1'>
+        <Stepper steps={steps} onStepClicked={setCurrentAndSubsequentSteps} />
+        <Button
+          label='Go to Step 2'
+          type='button'
+          onClick={() => setCurrentAndSubsequentSteps(2)}
+        />
+        <Button
+          label='Go to Step 11'
+          type='button'
+          onClick={() => setCurrentAndSubsequentSteps(11)}
+        />
+      </div>
+    </>
   );
 }
 
