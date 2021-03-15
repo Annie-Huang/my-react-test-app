89259e71bc6212a535f18f0f5a4a649464eda7fe
Annie Huang <annie.huang@auspost.com.au>
2021-03-15 16:44:12 +1100

    Test datalist and table

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,13 @@ import {
   Stepper,
   StepStatus,
   Button,
+  DataList,
+  useDatalistStateConsumer,
+  Alignment,
+  FieldType,
+  RowData,
 } from 'otc-ui-component-library';
+import { format as dateFnsFormat } from 'date-fns';
 
 const longStepList = [
   {
@@ -64,6 +70,172 @@ const longStepList = [
   },
 ];
 
+const dataListArgs = {
+  multiRowsSelectable: true,
+  rows: [
+    { id: 'jon', name: 'Jon', online: 'N', cost: 1500 },
+    { id: 'kara', name: 'Kara', online: 'Y', cost: 1400 },
+    { id: 'gerard', name: 'Gerard', online: 'Y', cost: 1300 },
+    { id: 4, name: 'Tim', online: 'N', cost: 1200 },
+  ],
+  columnConfigs: [
+    { field: 'name' },
+    { field: 'online', format: (v: string) => (v === 'Y' ? 'Yes' : 'No') },
+    { field: 'cost', format: (v: any) => `$${v}`, alignment: Alignment.Right },
+  ],
+  rowConfigs: [
+    {
+      id: 'jon',
+      hint: 'Pick this guy',
+      initialSelected: true,
+      keyboardShortcutKey: 'J',
+    },
+    {
+      id: 'gerard',
+      contextualHelp: { title: 'Notice', body: 'Developer/Investor' },
+      initialSelected: true,
+    },
+    { id: 4, initialDisabled: true },
+    { id: 'jon1', initialDisabled: true },
+  ],
+  focusFirstRowOnMount: true,
+};
+
+const tableArgs = {
+  multiRowsSelectable: true,
+  rows: [
+    {
+      id: 'jon',
+      name: 'Jon',
+      no: 9,
+      date0: '2020-11-18T13:54:51+10:00',
+      date1: '2020-11-18T13:54:51+10:00',
+      date2: '2 to 4 days',
+      cost: 1500,
+    },
+    {
+      id: 'kara',
+      name: 'Kara',
+      no: 8,
+      date0: '2020-09-18T13:52:51+10:00',
+      date1: '2020-09-18T13:52:51+10:00',
+      date2: 'Next day',
+      cost: 1400,
+    },
+    {
+      id: 'gerard',
+      name: 'Gerard',
+      no: 7,
+      date0: '2020-01-18T13:54:51+10:00',
+      date1: '2020-01-18T13:54:51+10:00',
+      date2: '2 to 4 days',
+      cost: 1300,
+    },
+    {
+      id: 4,
+      name: 'Tim B',
+      no: 6,
+      date0: '2020-09-18T13:52:51+10:00',
+      date1: '2020-09-18T13:52:51+10:00',
+      date2: 'Next day',
+      cost: 1200,
+    },
+    {
+      id: 'tim n',
+      name: 'Tim N',
+      no: 5,
+      date0: '2020-08-18T13:52:51+10:00',
+      date1: '2020-08-18T13:52:51+10:00',
+      date2: 'Next day',
+      cost: 1100,
+    },
+    {
+      id: 'damon',
+      name: 'Damon',
+      no: 4,
+      date0: '2020-07-18T13:52:51+10:00',
+      date1: '2020-07-18T13:52:51+10:00',
+      date2: '2 to 4 days',
+      cost: 1000,
+    },
+    {
+      id: 'nelson',
+      name: 'Nelson',
+      no: 3,
+      date0: '2020-06-18T13:52:51+10:00',
+      date1: '2020-06-18T13:52:51+10:00',
+      date2: 'Next day',
+      cost: 800,
+    },
+    {
+      id: 'michael',
+      name: 'Michael',
+      no: 2,
+      date0: '2020-05-18T13:52:51+10:00',
+      date1: '2020-05-18T13:52:51+10:00',
+      date2: '2 to 4 days',
+      cost: 700,
+    },
+    {
+      id: 'brett',
+      name: 'Brett',
+      no: 1,
+      date0: '2020-12-18T13:52:51+10:00',
+      date1: '2020-12-18T13:52:51+10:00',
+      date2: '2 to 4 days',
+      cost: 700,
+    },
+  ],
+  columnConfigs: [
+    { field: 'name', headerName: 'user name', fieldType: FieldType.String },
+    {
+      field: 'no',
+      headerName: 'employee no',
+      fieldType: FieldType.Number,
+    },
+    {
+      field: 'date0',
+      headerName: 'time',
+      fieldType: FieldType.Date,
+      format: (date: string) => dateFnsFormat(new Date(date), 'hh:mmaa'),
+    },
+    {
+      field: 'date1',
+      headerName: 'created at',
+      fieldType: FieldType.Date,
+      alignment: Alignment.Right,
+    },
+    {
+      field: 'date2',
+      headerName: 'delivery',
+      fieldType: FieldType.String,
+      alignment: Alignment.Right,
+    },
+    {
+      field: 'cost',
+      headerName: 'amount',
+      fieldType: FieldType.Currency,
+      alignment: Alignment.Right,
+    },
+  ],
+  rowConfigs: [
+    {
+      id: 'jon',
+      hint: 'Pick this guy',
+      initialSelected: true,
+      keyboardShortcutKey: 'J',
+    },
+    {
+      id: 'gerard',
+      contextualHelp: { title: 'Notice', body: 'Developer/Investor' },
+      initialSelected: true,
+    },
+    { id: 4, initialDisabled: true },
+    { id: 'jon1', initialDisabled: true },
+  ],
+  focusFirstRowOnMount: true,
+};
+
 function App() {
   /*  return (
     <div>
@@ -104,6 +276,7 @@ function App() {
     </div>
   );*/
 
+  /*
   let initialSteps = [...longStepList];
   const [steps, setSteps] = useState(initialSteps);
 
@@ -135,7 +308,71 @@ function App() {
         />
       </div>
     </>
+  );*/
+
+  /*
+  return (
+    <div style={{ width: 740, margin: '0 auto', marginTop: '100px' }}>
+      <DataList {...dataListArgs} />
+      <button>Click me</button>
+    </div>
   );
+*/
+
+  let updatedRowConfig = dataListArgs.rowConfigs.map((item) => ({ ...item }));
+  updatedRowConfig[0].initialSelected = undefined;
+  updatedRowConfig[1].initialSelected = undefined;
+
+  const datalistState: any = useDatalistStateConsumer();
+
+  const annieClick = () => {
+    console.log(
+      '~~~ datalistState.current?.selectedRowIds=',
+      datalistState.current?.selectedRowIds
+    );
+  };
+
+  const rowClicked = (currentSelectedRowIds: any) => {
+    console.log('annieannie');
+
+    console.log(
+      '$$$$$ rowClicked currentSelectedRowIds=',
+      currentSelectedRowIds
+    );
+    console.log(
+      '$$$$$ rowClicked datalistState.current?.selectedRowIds1=',
+      datalistState.current?.selectedRowIds
+    );
+  };
+
+  const tableHeaderClicked = (rows: any) => {
+    console.log('onTableHeaderClicked');
+    console.log('rows=', rows);
+  };
+
+  return (
+    <div style={{ width: 740, margin: '0 auto' }}>
+      <DataList
+        {...dataListArgs}
+        rowConfigs={updatedRowConfig}
+        onRowClicked={rowClicked}
+        ref={datalistState}
+      />
+      <button onClick={annieClick}>NextStep</button>
+    </div>
+  );
+
+  // return (
+  //   <div style={{ margin: '0 auto' }}>
+  //     <DataList
+  //       {...tableArgs}
+  //       onRowClicked={rowClicked}
+  //       onTableHeaderClicked={tableHeaderClicked}
+  //       ref={datalistState}
+  //     />
+  //     <button onClick={annieClick}>NextStep</button>
+  //   </div>
+  // );
 }
 
 export default App;
